e5a3c47b99f85ad1a41f48991545b7dac5927e37
Marin Allwin <marin.allwin@startup2launch.com>
2024-07-15 11:54:15 +0530

    redis test

---
+++ src/main/java/com/example/cluod_build_test/RedisService.java
@@ -0,0 +1,28 @@
+package com.example.cluod_build_test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    public void setValue(String key, String value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public String getValue(String key) {
+        if (redisTemplate.hasKey(key)) {
+            return redisTemplate.opsForValue().get(key);
+        } else {
+            return "Value does not exist";
+        }
+    }
+
+    public void deleteValue(String key) {
+        redisTemplate.delete(key);
+    }
+}
--- src/main/java/com/example/cluod_build_test/controller/MainController.java
+++ src/main/java/com/example/cluod_build_test/controller/MainController.java
@@ -1,10 +1,14 @@
 package com.example.cluod_build_test.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.cluod_build_test.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
+
+    @Autowired
+    private RedisService redisService;
 
     @GetMapping("/")
     public String hello() {
@@ -24,5 +28,20 @@ public class MainController {
     @GetMapping("/ask")
     public String ask() {
         return "How is your day....?";
+    }
+
+    @PostMapping("/set")
+    public void setValue(@RequestParam String key, @RequestParam String value) {
+        redisService.setValue(key, value);
+    }
+
+    @GetMapping("/get")
+    public String getValue(@RequestParam String key) {
+        return redisService.getValue(key);
+    }
+
+    @DeleteMapping("/delete")
+    public void deleteValue(@RequestParam String key) {
+        redisService.deleteValue(key);
     }
 }
